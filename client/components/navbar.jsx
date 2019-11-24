a5c6c24f76432a6d13c7061b6315fd68ccf6595f
Uzair Ashraf <uzinatorcl@live.com>
2019-11-23 19:36:56 -0800

    will continue tomorrow'

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 export default class Navbar extends React.Component {
   render() {
+    const handleClick = () => {
+      this.props.history.push('/signup');
+    };
     return (
       <div className="navbar">
         <img src="assets/images/logo.png" alt="Watch flix logo" />
-        <button className="home-page-signin">Sign In</button>
+        <button onClick={handleClick} className="home-page-signin">Sign In</button>
       </div>
     );
   }
