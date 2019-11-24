21cfc070debf9679ff71702edf6c617575a2a193
Uzair Ashraf <uzinatorcl@live.com>
2019-11-24 15:32:41 -0800

    psql db is setup and connected, users endpoint is also created

--- client/components/signin.jsx
+++ client/components/signin.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import axios from 'axios';
 import Navbar from './navbar';
 
 export default class SignIn extends React.Component {
+  componentDidMount() {
+    axios.get('/api/users')
+      .then(res => console.log(res));
+  }
   render() {
     return (
       <>
--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const path = require('path');
+const db = require('./database');
 const app = express();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/users', (req, res, next) => {
+  console.log('request incoming');
+  const sql = `
+    select "user_id",
+           "username"
+      from "users"
+  `;
+  db.query(sql)
+    .then(result => {
+      res.json(result.rows);
+    })
+    .catch(error => next(error));
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}`);
