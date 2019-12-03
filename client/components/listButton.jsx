7306829368a76fd3f1e4be9bff0e48d57d026e6f
Uzair Ashraf <uzinatorcl@live.com>
2019-12-02 22:28:10 -0800

    added material ui and icons for my list, creating endpoint to add items to list now

--- client/components/listButton.jsx
+++ client/components/listButton.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import CheckIcon from '@material-ui/icons/Check';
+import AddIcon from '@material-ui/icons/Add';
 
 class ListButton extends React.Component {
   render() {
-    const message = this.props.inList
-      ? 'Remove from List'
-      : 'Add to List';
+    const icon = this.props.inList
+      ? <CheckIcon/>
+      : <AddIcon/>;
     return (
-      <button className="add-list-button">{message}</button>
+      <button className="add-list-button">{icon} My List</button>
     );
   }
 }
